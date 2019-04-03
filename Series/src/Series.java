6cf530c4502a5a49062dfb36bbb8d5d3a160073e
pablobernalm <49001136+pablobernalm@users.noreply.github.com>
2019-04-03 20:01:09 +0200

    Add files via upload

---
+++ Series/src/Series.java
@@ -0,0 +1,34 @@
+import java.io.*;
+public class Series implements Entregable{
+
+	private String titulo, genero;
+	private int temporadas;
+	private boolean entregado;
+
+	
+
+	
+	public void entregar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void devolver() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void isEntregado() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void compareTo(Object a) {
+		// TODO Auto-generated method stub
+		
+ 	}
+
+}
